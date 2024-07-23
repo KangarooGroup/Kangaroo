25a5c1300118754c868903bc1dbfdddaaa1bae7e
Jie Hu <hujiemr@163.com>
2024-07-23 15:54:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,9 @@ def init_model():
     return tokenizer, model, terminators
 
 def show_video():
-    st.title('Kangraoo Chatbot')
-    path_or_url = st.text_input('Please enter the video path or URL. You can also use one of the demo videos available in the ./demo_videos directory: bike.mp4, castle.mp4, robot.mp4, or winter.mp4.', value='demo_videos/bike.mp4')
+    path_or_url = st.text_input('Please enter the video path or URL. You can also use one of \
+        the demo videos available in the ./demo_videos directory: bike.mp4, castle.mp4, \
+        robot.mp4, or winter.mp4.', value='demo_videos/bike.mp4')
     if path_or_url.strip() != '':
         if path_or_url.startswith('http'):
             video_path = f"demo_videos/{path_or_url.split('/')[-1]}"
@@ -27,7 +28,7 @@ def show_video():
         else:
             video_path = path_or_url
             assert Path(video_path).exists()
-        st.video(open(video_path, 'rb'))
+        st.video(open(video_path, 'rb'), autoplay=False, loop=True)
     return video_path
 
 def get_response(user_question, tokenizer, model, terminators, video_path):
@@ -52,38 +53,50 @@ def get_response(user_question, tokenizer, model, terminators, video_path):
         return response
     return None
 
-def display_history_conversation():
-    for item in st.session_state.message:
-        if item["role"] == "user":
-            st.write(f":blue[**[User]:** {item['content']}]")
-        else:
-            st.write(f":green[**[Kangaroo]:** {item['content']}]")
+def display_history_conversation(messages_widgt):    
+    with messages_widgt:
+        for item in st.session_state.message:
+            if item["role"] == "user":
+                messages_widgt.chat_message("user").write(f":blue[{item['content']}]")
+            else:
+                messages_widgt.chat_message("assistant").write(f":green[{item['content']}]")
       
 def chat(tokenizer, model, terminators, video_path):
     if "message" not in st.session_state:
         st.session_state.message = []
+    with st.sidebar:
+        st.write(r"$\textsf{\Large Conversation}$")
+        messages = st.container(height=700)
 
-    question_widget, send_button = st.columns([9,1], vertical_alignment='bottom')
-    user_question = question_widget.text_input("Your question:", key="input")
-    regen_button, clear_button = st.columns(2)
-    # click Send button
-    if send_button.button("Send", type="primary"):
-        response = get_response(user_question, tokenizer, model, terminators, video_path)
-        display_history_conversation()
+    question_input, regen_button, clear_button = st.columns([7,1,1], gap='small')
+    with question_input:
+        if user_question := st.chat_input("Enter question"):
+            response = get_response(user_question, tokenizer, model, terminators, video_path)
+            display_history_conversation(messages)
+    
     # click Regenerate button
-    if regen_button.button("Regenerate"):
+    if regen_button.button("Regen"):
         st.session_state.message.pop()
         user_question = st.session_state.message[-1]["content"]
         st.session_state.message.pop()
         response = get_response(user_question, tokenizer, model, terminators, video_path)
-        display_history_conversation()
-    # click Clear history button
-    if clear_button.button("Clear history"):
+        display_history_conversation(messages)
+    # click Reset button
+    if clear_button.button("Reset", type='primary'):
         st.session_state.message = []
 
+def init_st_page():
+    st.set_page_config(page_title='Kangraoo', page_icon='assets/logo.png', 
+                       layout="centered", initial_sidebar_state="expanded")
+    widgts = st.columns([1, 2], vertical_alignment='bottom')
+    with widgts[0]:
+        st.image('assets/logo.png', width=128)
+    with widgts[1]:
+        st.title("Kangraoo Chatbot")
+    
             
 if __name__ == "__main__":
-    st.sidebar.image('assets/logo.png')
+    init_st_page()
     tokenizer, model, terminators = init_model()
     video_path = show_video()
     chat(tokenizer, model, terminators, video_path)
